6fac69172edac61cc893070578fba6881484e236
Fred Bricon <fbricon@gmail.com>
2014-09-08 11:42:00 -0400

    Remove unused imports

--- jboss-forge-html5-archetype/src/main/resources/archetype-resources/src/main/java/model/Member.java
+++ jboss-forge-html5-archetype/src/main/resources/archetype-resources/src/main/java/model/Member.java
@@ -22,8 +22,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
